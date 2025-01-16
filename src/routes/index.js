3a2bb39dc53125a71b3a347f8d3f2777cd8966b7
ALP300 <aitorpalomino1@gmail.com>
2025-01-16 18:58:02 -0500

    Actualizar proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,5 @@ router.get('/about' , (req, res)=>res.render('about',{title:'Acerca de nosotros'
 router.get('/contact', (req, res)=>res.render('contact',{title:'Contacto'}));
 router.get('/products', (req, res)=>res.render('products',{title:'Productos'}));
 router.get('/promociones', (req, res)=>res.render('promociones',{title:'Promociones'}));
+router.get('/bot', (req, res)=>res.render('bot',{title:'Bot'}));
 export default router;
