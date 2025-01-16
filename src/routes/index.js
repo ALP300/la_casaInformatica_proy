218867eaaca230b19dac551e909534185484e00d
ALP300 <aitorpalomino1@gmail.com>
2025-01-16 18:32:26 -0500

    creacion de archivos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,7 @@ import { Router } from "express";
 const router= Router();
 router.get('/', (req, res)=>res.render('index',{title:'La Casa Informática'}));
 router.get('/about' , (req, res)=>res.render('about',{title:'Acerca de nosotros'}));
-
+router.get('/contact', (req, res)=>res.render('contact',{title:'Contacto'}));
+router.get('/products', (req, res)=>res.render('products',{title:'Productos'}));
+router.get('/promociones', (req, res)=>res.render('promociones',{title:'Promociones'}));
 export default router;
