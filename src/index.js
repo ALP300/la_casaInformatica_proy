8051bac61969b412bee7e67c82de0a982d39a7ce
ALP300 <aitorpalomino1@gmail.com>
2025-01-21 17:12:56 -0500

    Realizar proyecto

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ const __dirname= dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(router)
+app.use(express.static(join(__dirname, 'public')));
 app.listen(3000)
 console.log('Server is running on port 3000')
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router= Router();
 router.get('/', (req, res)=>res.render('index',{title:'La Casa Informática'}));
 router.get('/about' , (req, res)=>res.render('about',{title:'Acerca de nosotros'}));
-router.get('/contact', (req, res)=>res.render('contact',{title:'Contacto'}));
+router.get('/servicios', (req, res)=>res.render('servicios',{title:'Servicios'}));
 router.get('/products', (req, res)=>res.render('products',{title:'Productos'}));
 router.get('/promociones', (req, res)=>res.render('promociones',{title:'Promociones'}));
 router.get('/bot', (req, res)=>res.render('bot',{title:'Bot'}));
